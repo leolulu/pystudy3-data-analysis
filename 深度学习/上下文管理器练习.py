1245dffeb10684cc354020b4e37be090095a8bca
leolulu <348699103@qq.com>
2018-11-22 10:18:10 +0800

    Create 上下文管理器练习.py

---
+++ 深度学习/上下文管理器练习.py
@@ -0,0 +1,26 @@
+class Baseclass:
+    def __init__(self):
+        self.mark = 'this is base instance!'
+
+    def bark(self):
+        print(
+            'my mark is :',
+            self.mark
+        )
+
+    def err_func(self):
+        num = 1/0
+
+
+class ContextManager:
+    def __enter__(self):
+        print('进入上下文管理器')
+        return Baseclass()
+
+    def __exit__(self,type1,val1,tb1):
+        print(type1,val1,tb1,sep='+++++++++++++')
+        print('退出上下文管理器')
+
+
+with ContextManager() as f:
+    f.err_func()
