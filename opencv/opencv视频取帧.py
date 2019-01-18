1ed5872675a186ca4b3a0db449e8094ee9ca671a
leolulu <348699103@qq.com>
2019-01-18 15:50:21 +0800

    update

--- opencv/opencv视频取帧.py
+++ opencv/opencv视频取帧.py
@@ -3,7 +3,7 @@ import cv2
 
 
 def cutFrame(file_path, fps_rate=1):
-    file_name_list = list(filter(lambda x: x[-3:] == 'mkv', os.listdir(file_path)))
+    file_name_list = filter(lambda x: x[-3:] == 'mkv', os.listdir(file_path))
 
     for media_file_path in [{'file_name': file_name, 'path': os.path.join(file_path, file_name)} for file_name in file_name_list]:
         dir_name = './save_imgs/{}'.format(media_file_path['file_name'].split('.')[0])
@@ -23,10 +23,10 @@ def cutFrame(file_path, fps_rate=1):
                 print(frame_count)
             success, frame = cap.read()
             if frame_count % (fps * fps_rate) == 0:
-                cv2.imencode(".jpg",frame)[1].tofile(dir_name+'/{}.jpg'.format(frame_count))
+                cv2.imencode(".jpg", frame)[1].tofile(dir_name+'/{}.jpg'.format(frame_count))
             frame_count += 1
         cap.release()
 
 
 if __name__ == '__main__':
-    cutFrame(r'F:\迅雷下载\[アニメ BD] クイーンズブレイド(QB) 第1-3期+OVAsx2+OAD 全48話+特典+CDx24(1920x1080 HEVC 10bit FLAC softSub(chi+eng) chap)')
+    cutFrame(r'F:\迅雷下载\[アニメ BD] クイーンズブレイド(QB) 第1-3期+OVAsx2+OAD 全48話+特典+CDx24(1920x1080 HEVC 10bit FLAC softSub(chi+eng) chap)', 3)
