d23f72136b317bbf2f756504d5269e08e63606a2
leolulu <348699103@qq.com>
2019-01-18 10:01:23 +0800

    Update opencv视频取帧.py

--- opencv/opencv视频取帧.py
+++ opencv/opencv视频取帧.py
@@ -6,7 +6,6 @@ def cutFrame(file_path, fps_rate=1):
     file_name_list = list(filter(lambda x: x[-3:] == 'mkv', os.listdir(file_path)))
 
     for media_file_path in [{'file_name': file_name, 'path': os.path.join(file_path, file_name)} for file_name in file_name_list]:
-        print(media_file_path['file_name'])
         dir_name = './save_imgs/{}'.format(media_file_path['file_name'].split('.')[0])
         print(dir_name)
         try:
@@ -24,7 +23,6 @@ def cutFrame(file_path, fps_rate=1):
                 print(frame_count)
             success, frame = cap.read()
             if frame_count % (fps * fps_rate) == 0:
-                print(success,frame)
                 cv2.imencode(".jpg",frame)[1].tofile(dir_name+'/{}.jpg'.format(frame_count))
             frame_count += 1
         cap.release()
