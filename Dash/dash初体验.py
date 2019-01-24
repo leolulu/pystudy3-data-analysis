af6dfcca09c265ca04eb28eb839d33d4f99efb90
leolulu <348699103@qq.com>
2019-01-24 13:49:44 +0800

    dash

---
+++ Dash/dash初体验.py
@@ -0,0 +1,26 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    html.H1('Hello Dash',id='header1'),
+    html.H2('第二个H',className='header2'),
+    html.Div(' Dash: A web application framework for Python.'),
+    dcc.Graph(
+        id='first-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout':{
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(port=33567, debug=True)
